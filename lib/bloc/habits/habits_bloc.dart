d0e080be68483c90ae7457a1a906e88b8e0f80ce
Brett Owers <bowers@Bretts-MacBook-Pro.local>
2023-11-16 17:17:46 -0700

    day to datetime

--- lib/bloc/habits/habits_bloc.dart
+++ lib/bloc/habits/habits_bloc.dart
@@ -36,22 +36,26 @@ class HabitsBloc extends Bloc<HabitsEvent, HabitsState> {
       return [...previousValue, ...element.habitEntries];
     });
 
-    List<HabitEntry> threeDaysAgoEntries =
-        habitEntries.where((element) => element.createDate.day == now.subtract(const Duration(days: 3)).day).toList();
-    List<HabitEntry> twoDaysAgoEntries =
-        habitEntries.where((element) => element.createDate.day == now.subtract(const Duration(days: 2)).day).toList();
-    List<HabitEntry> aDayAgoEntries = habitEntries.where((element) => element.createDate.day == now.subtract(const Duration(days: 1)).day).toList();
+    DateTime threeDaysAgo = now.subtract(const Duration(days: 3));
+    DateTime twoDaysAgo = now.subtract(const Duration(days: 2));
+    DateTime aDayAgo = now.subtract(const Duration(days: 1));
+    DateTime tomorrow = now.add(const Duration(days: 1));
+    DateTime twoDaysFromNow = now.add(const Duration(days: 2));
+    DateTime threeDaysFromNow = now.add(const Duration(days: 3));
+
+    List<HabitEntry> threeDaysAgoEntries = habitEntries.where((element) => element.createDate.day == threeDaysAgo.day).toList();
+    List<HabitEntry> twoDaysAgoEntries = habitEntries.where((element) => element.createDate.day == twoDaysAgo.day).toList();
+    List<HabitEntry> aDayAgoEntries = habitEntries.where((element) => element.createDate.day == aDayAgo.day).toList();
     List<HabitEntry> todaysEntries = habitEntries.where((element) => element.createDate.day == now.day).toList();
-    List<HabitEntry> tomorrowEntries = habitEntries.where((element) => element.createDate.day == now.add(const Duration(days: 1)).day).toList();
-    List<HabitEntry> twoDaysFromNowEntries = habitEntries.where((element) => element.createDate.day == now.add(const Duration(days: 2)).day).toList();
-    List<HabitEntry> threeDaysFromNowEntries =
-        habitEntries.where((element) => element.createDate.day == now.add(const Duration(days: 3)).day).toList();
+    List<HabitEntry> tomorrowEntries = habitEntries.where((element) => element.createDate.day == tomorrow.day).toList();
+    List<HabitEntry> twoDaysFromNowEntries = habitEntries.where((element) => element.createDate.day == twoDaysFromNow.day).toList();
+    List<HabitEntry> threeDaysFromNowEntries = habitEntries.where((element) => element.createDate.day == threeDaysFromNow.day).toList();
 
     // await _backFillHabitEntries(3, habits, threeDaysAgoEntries);
     // await _backFillHabitEntries(2, habits, twoDaysAgoEntries);
-    await _backFillHabitEntries(1, habits, aDayAgoEntries);
-    await _backFillHabitEntries(0, habits, todaysEntries);
-    await _backFillHabitEntries(-1, habits, tomorrowEntries);
+    await _backFillHabitEntries(aDayAgo, habits, aDayAgoEntries);
+    await _backFillHabitEntries(now, habits, todaysEntries);
+    await _backFillHabitEntries(tomorrow, habits, tomorrowEntries);
     // await _backFillHabitEntries(-2, habits, twoDaysFromNowEntries);
     // await _backFillHabitEntries(-3, habits, threeDaysFromNowEntries);
 
@@ -60,7 +64,7 @@ class HabitsBloc extends Bloc<HabitsEvent, HabitsState> {
     emit(HabitsLoaded(habitEntities));
   }
 
-  Future _backFillHabitEntries(int day, List<Habit> habits, List<HabitEntry> entries) async {
+  Future _backFillHabitEntries(DateTime day, List<Habit> habits, List<HabitEntry> entries) async {
     // if you have less entries than habits, create entries for the missing habits
     // right now it's creating entries for all habits
     // -TODO-: only create entries for habits that don't have entries
@@ -68,7 +72,7 @@ class HabitsBloc extends Bloc<HabitsEvent, HabitsState> {
     if (entries.length < habits.length) {
       for (var habit in habits) {
         HabitEntry t = HabitEntry.fromHabit(habit);
-        DateTime updatedDate = DateTime.now().subtract(Duration(days: day));
+        DateTime updatedDate = DateTime.now().subtract(Duration(days: day.day));
         t = t.copyWith(updateDate: updatedDate, createDate: updatedDate);
         await habitEntryRepository.createIfDoesntExistForDate(t);
       }
